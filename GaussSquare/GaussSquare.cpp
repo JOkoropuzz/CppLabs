40218c3089b8cd8877b4a4dd01820fd26300cad4
AdskayaMashinka <tank_nikcf@mail.ru>
2024-07-11 00:15:33 +0300

    Добавьте файлы проекта.

---
+++ GaussSquare/GaussSquare.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include<array>
+using namespace std;
+
+class Point
+{
+private:
+    double x, y;
+
+public:
+    Point() : x(0), y(0) {}
+    Point(double xVal, double yVal) : x(xVal), y(yVal) {}
+
+    int getX() const { return x; }
+    int getY() const { return y; }
+
+    void setY(double value) { y = value; }
+    void setX(double value) { x = value; }
+
+    void inputPoint()
+    {
+        double a, b;
+        cout << "Введите координаты X и Y вершины через пробел: ";
+        cin >> a >> b;
+        setX(a);
+        setY(b);
+
+    }
+};
+
+static double GaussSqare(array<Point, 5> points)
+{
+    double result = abs(points[0].getX() * points[1].getY() + points[1].getX() * points[2].getY()
+        + points[2].getX() * points[3].getY() + points[3].getX() * points[4].getY()
+        + points[4].getX() * points[0].getY() - points[0].getY() * points[1].getX()
+        - points[1].getY() * points[2].getX() - points[2].getY() * points[3].getX()
+        - points[3].getY() * points[4].getX() - points[4].getY() * points[0].getX()) / 2.0;
+    return result;
+}
+
+int main()
+{
+
+    system("chcp 1251");
+    cout << "Расчёт площяди пятиугольника по формуле Гаусса" << endl;
+
+
+    Point p1, p2, p3, p4, p5;
+    array<Point, 5> points = { p1, p2, p3, p4, p5 };
+
+    for (int i = 0; i < points.size(); i++)
+    {
+        points[i].inputPoint();
+    }
+
+    cout << "Площадь пятиугольника:" << GaussSqare(points) << endl;
+}
---
+++ SuperPrimeNumber/SuperPrimeNumber.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+static vector<bool> SieveOfEratosthenes(int n)
+{
+    // Create a boolean array "prime[0..n]" and initialize
+    // all entries it as true. A value in prime[i] will
+    // finally be false if i is Not a prime, else true.
+
+
+    vector<bool> prime(n + 1, true);
+
+
+    for (int p = 2; p * p <= n; p++) {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true) {
+            // Update all multiples of p greater than or
+            // equal to the square of it numbers which are
+            // multiple of p and are less than p^2 are
+            // already been marked.
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    return prime;
+}
+
+static bool IsPrime(int n)
+{
+    bool result = false;
+    if (n > 1)
+    {
+        result = true;
+        for (int i = 2; i <= sqrt(n); i++)
+        {
+            // If n is divisible by any number between 
+            // 2 and n/2, it is not prime 
+            if (n % i == 0)
+            {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+static bool IsSuperPrime(int n)
+{
+    bool result = false;
+    if (IsPrime(n))
+    {
+
+        int p = 0;
+        for (int i = 2; i < size(SieveOfEratosthenes(n)); i++)
+        {
+            if (SieveOfEratosthenes(n)[i] == true)
+            {
+                p++;
+            }
+        }
+        if (IsPrime(p))
+        {
+            result = true;
+        }
+    }
+    return  result;
+}
+
+
+
+int main()
+{
+    system("chcp 1251");
+    int a;
+    cout << "Введите положительное число: ";
+    cin >> a;
+    if (IsSuperPrime(a))
+    {
+        cout << "Число " << a << " является Супер Простым!";
+    }
+    else
+    {
+        cout << "Число " << a << " НЕ является Супер Простым!";
+    }
+}
+
+
